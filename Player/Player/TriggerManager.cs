023910f3bb4245aba17ec65f6604367be1aec32b
SilentSeto <103815100+SilentSeto@users.noreply.github.com>
2022-06-24 22:06:37 +0700

    Add files via upload

---
+++ Player/Player/TriggerManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager : MonoBehaviour {
+
+    public string ExitTargetTagName = "Exit";
+
+    public UnityEngine.Events.UnityEvent OnMazeEnds;
+    public UnityEngine.Events.UnityEvent OnMazeEndFail;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with something");
+
+
+        if (other.tag == "Lava")
+        {
+            this.GetComponent<Animator>().SetTrigger("DieNow");
+            OnMazeEndFail.Invoke();
+
+        }
+
+
+
+
+        if (other.tag == ExitTargetTagName)
+        {
+            Debug.Log("well done. you exited the maze");
+            OnMazeEnds.Invoke();
+        }
+    }
+}
---
+++ Script/Move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move: MonoBehaviour{
+	Animator myAnimator;
+	public float speed = 5.0f;
+	public float gravity = 20.0f;
+	private Vector3 moveDirection = Vector3.zero;
+	CharacterController controller;
+	float currSpeed, param1;
+
+	void Start(){
+		controller = GetComponent<CharacterController>();
+		myAnimator = GetComponent<Animator>();   
+	}
+		
+	void Update(){
+		param1 = 0;
+		if(controller.isGrounded){
+			currSpeed = speed;
+			param1 = Mathf.Abs(Input.GetAxis("Vertical"));
+			myAnimator.SetFloat("Speed", param1);
+
+			transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+			myAnimator.SetFloat("Speed", param1);
+
+			moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= currSpeed;
+		}
+		if(!myAnimator.IsInTransition(0)){
+			moveDirection.y -= gravity * Time.deltaTime;
+			controller.Move(moveDirection * Time.deltaTime);
+		}
+	}
+}
